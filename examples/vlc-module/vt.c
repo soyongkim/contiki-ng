53f66fe9008f6c5f44823d312473aafbdc20c2ea
soyongkim <thdyd324@gmail.com>
2020-01-29 14:11:59 +0900

    test broadcast

--- examples/vlc-module/vt.c
+++ examples/vlc-module/vt.c
@@ -46,11 +46,23 @@ change_target(int node_id) {
     const uip_ipaddr_t *default_prefix = uip_ds6_default_prefix();
     uip_ipaddr_t dest_ipaddr;
     uip_ip6addr_copy(&dest_ipaddr, default_prefix);
-      
-    dest_ipaddr.u16[4] = UIP_HTONS(0x0212);
-    dest_ipaddr.u16[5] = UIP_HTONS(0x7400 + node_id);
-    dest_ipaddr.u16[6] = UIP_HTONS(0x0000 + node_id);
-    dest_ipaddr.u16[7] = UIP_HTONS(0x0000 + (256)*node_id + node_id);
+
+    if(node_id == 0) {
+      dest_ipaddr.u16[0] = UIP_HTONS(0xFF02);
+      dest_ipaddr.u16[1] = UIP_HTONS(0x0000);
+      dest_ipaddr.u16[2] = UIP_HTONS(0x0000);
+      dest_ipaddr.u16[3] = UIP_HTONS(0x0000); 
+      dest_ipaddr.u16[4] = UIP_HTONS(0x0000);
+      dest_ipaddr.u16[5] = UIP_HTONS(0x0000);
+      dest_ipaddr.u16[6] = UIP_HTONS(0x0000);
+      dest_ipaddr.u16[7] = UIP_HTONS(0x0001);
+    }
+    else {
+      dest_ipaddr.u16[4] = UIP_HTONS(0x0212);
+      dest_ipaddr.u16[5] = UIP_HTONS(0x7400 + node_id);
+      dest_ipaddr.u16[6] = UIP_HTONS(0x0000 + node_id);
+      dest_ipaddr.u16[7] = UIP_HTONS(0x0000 + (256)*node_id + node_id);
+    }
 
     return dest_ipaddr;
 }
@@ -77,7 +89,7 @@ PROCESS_THREAD(udp_client_process, ev, data)
     if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
       /* Send to DAG root */
       /* 목적지 원하는 곳으로 바꾸기 */
-      dest_ipaddr = change_target(1);
+      dest_ipaddr = change_target(0);
       LOG_INFO("Sending request %u to ", count);
       LOG_INFO_6ADDR(&dest_ipaddr);
       LOG_INFO_("\n");
