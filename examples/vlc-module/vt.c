854165c2b54563f2d50ff6bf04b4dfdfb8f66422
soyongkim <thdyd324@gmail.com>
2020-01-29 10:50:04 +0900

    update vt unicast

--- examples/vlc-module/vt.c
+++ examples/vlc-module/vt.c
@@ -1,87 +1,92 @@
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
-
 #include "contiki.h"
-#include "simple-udp.h"
+#include "net/routing/routing.h"
+#include "random.h"
+#include "net/netstack.h"
+#include "net/ipv6/simple-udp.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/ipv6/uip-debug.h"
 
-#include <stdio.h> /* For printf() */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
 
-#define UDP_PORT 5564
+#define WITH_SERVER_REPLY  1
+#define UDP_CLIENT_PORT	8765
+#define UDP_SERVER_PORT	5678
 
+#define SEND_INTERVAL		  (60 * CLOCK_SECOND)
 
-static void udp_rx_callback(struct simple_udp_connection *c,
-                           const uip_ipaddr_t *sender_addr,
-                           uint16_t sender_port,
-                           const uip_ipaddr_t *receiver_addr,
-                           uint16_t receiver_port,
-                           const uint8_t *data,
-                           uint16_t datalen)
+static struct simple_udp_connection udp_conn;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "UDP client");
+AUTOSTART_PROCESSES(&udp_client_process);
+/*---------------------------------------------------------------------------*/
+static void
+udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
 {
-  printf("receive Addr: %d Port: %d\n", receiver_addr->u16, receiver_port);
+
+  LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
+  LOG_INFO_6ADDR(sender_addr);
+#if LLSEC802154_CONF_ENABLED
+  LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
+#endif
+  LOG_INFO_("\n");
+
+}
+
+bool is_vt() {
+  
 }
 
 /*---------------------------------------------------------------------------*/
-PROCESS(vt_process, "vt process");
-AUTOSTART_PROCESSES(&vt_process);
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(vt_process, ev, data)
+PROCESS_THREAD(udp_client_process, ev, data)
 {
-  static struct etimer timer;
-  static struct simple_udp_connection udp_conn;
-  uint8_t payload[64] = {"VT"};
-
-  simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, udp_rx_callback);
+  static struct etimer periodic_timer;
+  static unsigned count;
+  static char str[32];
+  const uip_ipaddr_t *default_prefix = uip_ds6_default_prefix();
+  uip_ipaddr_t dest_ipaddr;
 
   PROCESS_BEGIN();
 
-  /* Setup a periodic timer that expires after 10 seconds. */
-  etimer_set(&timer, CLOCK_SECOND * 10);
+  /* Initialize UDP connection */
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
+                      UDP_SERVER_PORT, udp_rx_callback);
 
+  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
   while(1) {
-    printf("I'm VT module\n");
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
-    // simple_udp_sendto_port(&udp_conn, payload, 2, )
+    if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+      /* Send to DAG root */
 
+      uip_ip6addr_copy(&dest_ipaddr, default_prefix);
+      
+      dest_ipaddr.u16[4] = UIP_HTONS(0x0212);
+      dest_ipaddr.u16[5] = UIP_HTONS(0x7401);
+      dest_ipaddr.u16[6] = UIP_HTONS(0x0001);
+      dest_ipaddr.u16[7] = UIP_HTONS(0x0101);
 
-    /* Wait for the periodic timer to expire and then restart the timer. */
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    etimer_reset(&timer);
+      LOG_INFO("Sending request %u to ", count);
+      LOG_INFO_6ADDR(&dest_ipaddr);
+      LOG_INFO_("\n");
+      snprintf(str, sizeof(str), "hello %d", count);
+      simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
+      count++;
+    } else {
+      LOG_INFO("Not reachable yet\n");
+    }
+
+    /* Add some jitter */
+    etimer_set(&periodic_timer, SEND_INTERVAL
+      - CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)));
   }
 
   PROCESS_END();
