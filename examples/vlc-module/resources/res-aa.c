63205fcecdb8eabf51f5d0651864f8fd58ee0110
soyongkim <thdyd324@gmail.com>
2020-04-28 15:03:41 +0900

    remove memset

--- examples/vlc-module/resources/res-aa.c
+++ examples/vlc-module/resources/res-aa.c
@@ -62,7 +62,7 @@ res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buf
   printf("LEN:%d\n", request->payload_len);
 
   vip_parse_message(vip_pkt, request->payload, request->payload_len);
-  //printf("TYPE:%d\n", vip_pkt->type);
-  //printf("AA-ID:%d\n", vip_pkt->aa_id);
-  //printf("VT-ID:%d\n", vip_pkt->vt_id);
+  printf("TYPE:%d\n", vip_pkt->type);
+  printf("AA-ID:%d\n", vip_pkt->aa_id);
+  printf("VT-ID:%d\n", vip_pkt->vt_id);
 }
