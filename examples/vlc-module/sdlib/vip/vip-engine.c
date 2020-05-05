befffaa4fb4b5d011e672e93678a57af6895e3ac
soyongkim <thdyd324@gmail.com>
2020-05-05 20:12:36 +0900

    Update vip-engine.c

--- examples/vlc-module/sdlib/vip/vip-engine.c
+++ examples/vlc-module/sdlib/vip/vip-engine.c
@@ -50,7 +50,7 @@ PROCESS_THREAD(vip_engine, ev, data)
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
     printf("VIP Engine Test Count %d\n", count++);
-    process_poll(aa_process);
+    process_poll(&aa_process);
     etimer_reset(&et);
     }
 
