aaac7279aab81ae9021e37a1d19b478e912129fc
soyongkim <thdyd324@gmail.com>
2020-05-10 16:46:54 +0900

    Update common.h

--- examples/vlc-module/sdlib/common.h
+++ examples/vlc-module/sdlib/common.h
@@ -8,11 +8,6 @@
  */
 uip_ip6addr_t change_target(int node_id);
 
-int make_basis_header(int* header, int type, int payload_length, int aa_id, int vt_id);
-
-int make_type_specific_header(int type);
-
 void make_coap_uri(char *src_coap_uri, int node_id);
-
 void sky_mote_address(uip_ipaddr_t *dest_ipaddr, int node_id);
-void cooja_mote_address(uip_ipaddr_t *dest_ipaddr, int node_id)
+void cooja_mote_address(uip_ipaddr_t *dest_ipaddr, int node_id);
