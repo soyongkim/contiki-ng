5efee622919f6424e5623d2e0a41eca4ea71e9d5
soyongkim <thdyd324@gmail.com>
2020-05-17 17:12:12 +0900

    test beaconing

--- examples/vlc-module/resources/res-vt.c
+++ examples/vlc-module/resources/res-vt.c
@@ -119,14 +119,15 @@ handler_beacon(vip_message_t *rcv_pkt) {
 /* beaconing */
 static void
 beconing() {
+  printf("test...");
   /* if the vt is complete to register to aa, start beaconing */
   if(aa_id) {
     printf("Beaconing...\n");
 
-    vip_init_message(snd_pkt, VIP_TYPE_BEACON, aa_id, vt_id);
-    make_coap_uri(set_uri, node_id);
-    vip_set_dest_ep(snd_pkt, set_uri, VIP_VR_URL);
-    vip_serialize_message(snd_pkt, buffer);
+    // vip_init_message(snd_pkt, VIP_TYPE_BEACON, aa_id, vt_id);
+    // make_coap_uri(set_uri, node_id);
+    // vip_set_dest_ep(snd_pkt, set_uri, VIP_VR_URL);
+    // vip_serialize_message(snd_pkt, buffer);
     //process_post(&vt_process, vt_snd_event, (void *)snd_pkt);
   }
 }
