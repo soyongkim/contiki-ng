bf0a00585d67089a5705bfbb324517a8c5036738
soyongkim <thdyd324@gmail.com>
2020-05-20 11:45:44 +0900

    Update vr.c

--- examples/vlc-module/vr.c
+++ examples/vlc-module/vr.c
@@ -91,8 +91,10 @@ vip_request(vip_message_t *snd_pkt) {
   coap_endpoint_parse(snd_pkt->dest_coap_addr, strlen(snd_pkt->dest_coap_addr), &dest_ep);
   coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
   coap_set_header_uri_path(request, snd_pkt->dest_path);
-  //coap_set_header_uri_host(request, snd_pkt->src_coap_addr);
+  coap_set_header_uri_host(request, snd_pkt->src_coap_addr);
   coap_set_payload(request, snd_pkt->buffer, snd_pkt->total_len);
+
+  printf("VR Request to %s(%d)\n", snd_pkt->dest_coap_addr, snd_pkt->type);
 
   coap_send_request(&callback_state, &dest_ep, request, vip_request_callback);
 }
