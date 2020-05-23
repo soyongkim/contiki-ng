d868bd252e321ab332601d7a7073e3ce6a72dfc7
soyongkim <thdyd324@gmail.com>
2020-05-23 17:27:19 +0900

    Update res-vr.c

--- examples/vlc-module/resources/res-vr.c
+++ examples/vlc-module/resources/res-vr.c
@@ -99,10 +99,14 @@ handler_beacon(vip_message_t *rcv_pkt) {
     /* Recent Received vt-id, aa-id */
     vip_init_message(snd_pkt, VIP_TYPE_VRR, aa_id, vt_id, vr_id);
     /* send to new aa */
-    vip_set_ep_cooja(snd_pkt, query, node_id, dest_addr, aa_id, VIP_AA_URL);
+    vip_set_dest_ep_cooja(snd_pkt,dest_addr, aa_id, VIP_AA_URL);
     vip_set_type_header_nonce(snd_pkt, 0);
     /* set vr id to 0. it's mean not allocated*/
     vip_serialize_message(snd_pkt, buffer);
+
+    vip_init_query(query);
+    vip_make_query_src(query, node_id);
+    vip_set_query(snd_pkt, query);
 
     vip_request(snd_pkt);
   } else {
@@ -195,11 +199,13 @@ static void
 vip_request(vip_message_t *snd_pkt) {
   /* set vip endpoint */
   coap_endpoint_parse(snd_pkt->dest_coap_addr, strlen(snd_pkt->dest_coap_addr), &dest_ep);
-  coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+  coap_init_message(request, COAP_TYPE_NON, COAP_POST, 0);
   coap_set_header_uri_path(request, snd_pkt->dest_path);
-  coap_set_header_uri_query(request, snd_pkt->query);
   coap_set_payload(request, snd_pkt->buffer, snd_pkt->total_len);
+
+  if(snd_pkt->query)
+    coap_set_header_uri_query(request, snd_pkt->query);
+
   printf("Send from %s to %s\n", snd_pkt->query, snd_pkt->dest_coap_addr);
   coap_send_request(&callback_state, &dest_ep, request, vip_request_callback);
 }
-
