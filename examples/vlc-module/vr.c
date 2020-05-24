c6bb6b6258d9c9f7a7cc2db3eac771c6cd7b3201
soyongkim <thdyd324@gmail.com>
2020-05-24 16:41:42 +0900

    Update vr.c

--- examples/vlc-module/vr.c
+++ examples/vlc-module/vr.c
@@ -25,7 +25,7 @@ extern vip_entity_t vr_type_handler;
 
 /* test event process */
 process_event_t vr_snd_event;
-int rcv_nonce;
+static int rcv_nonce;
 
 /* for send packet */
 static coap_callback_request_state_t callback_state;
