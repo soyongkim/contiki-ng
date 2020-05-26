b93c4a2743b9c61f91524151f96081eafb7c9e76
soyongkim <thdyd324@gmail.com>
2020-05-26 22:12:04 +0900

    Update vr.c

--- examples/vlc-module/vr.c
+++ examples/vlc-module/vr.c
@@ -88,7 +88,7 @@ timer_callback(void* data)
 static void init()
 {
   int random_incount;
-  random_incount = random_rand() % 1000;
+  random_incount = random_rand() % 1500;
   printf("Set Send Timer %d\n", random_incount);
 
   ctimer_set(&ct, random_incount, timer_callback, NULL);
