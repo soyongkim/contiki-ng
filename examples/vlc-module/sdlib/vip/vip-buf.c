b76238e26e8792bac4c5b77a289dde8504040913
soyongkim <thdyd324@gmail.com>
2020-05-26 21:13:48 +0900

    Update vip-buf.c

--- examples/vlc-module/sdlib/vip/vip-buf.c
+++ examples/vlc-module/sdlib/vip/vip-buf.c
@@ -9,38 +9,38 @@ vip_snd_node_t* make_vip_pkt_node(vip_message_t* vip_pkt)
 {
     vip_snd_node_t* node = calloc(1, sizeof(vip_snd_node_t));
 
-    node->next = NULL;
-    node->vip_pkt = calloc(1, sizeof(vip_message_t));
+    // node->next = NULL;
+    // node->vip_pkt = calloc(1, sizeof(vip_message_t));
 
-    /* dest ep info */
-    node->vip_pkt->dest_coap_addr = calloc(50, sizeof(char));
-    node->vip_pkt->dest_path = calloc(50, sizeof(char));
-    memcpy(node->vip_pkt->dest_coap_addr, vip_pkt->dest_coap_addr, strlen(vip_pkt->dest_coap_addr));
-    memcpy(node->vip_pkt->dest_path, vip_pkt->dest_path, strlen(vip_pkt->dest_path));
+    // /* dest ep info */
+    // node->vip_pkt->dest_coap_addr = calloc(50, sizeof(char));
+    // node->vip_pkt->dest_path = calloc(50, sizeof(char));
+    // memcpy(node->vip_pkt->dest_coap_addr, vip_pkt->dest_coap_addr, strlen(vip_pkt->dest_coap_addr));
+    // memcpy(node->vip_pkt->dest_path, vip_pkt->dest_path, strlen(vip_pkt->dest_path));
 
-    /* query */
-    if(vip_pkt->query_len)
-    {
-        node->vip_pkt->query_len = vip_pkt->query_len;
-        node->vip_pkt->query = calloc(50, sizeof(char));
-        memcpy(node->vip_pkt->query, vip_pkt->query, vip_pkt->query_len);
-    }
+    // /* query */
+    // if(vip_pkt->query_len)
+    // {
+    //     node->vip_pkt->query_len = vip_pkt->query_len;
+    //     node->vip_pkt->query = calloc(50, sizeof(char));
+    //     memcpy(node->vip_pkt->query, vip_pkt->query, vip_pkt->query_len);
+    // }
 
-    /* uplink */
-    int uplink_id_len = strlen(vip_pkt->uplink_id);
-    if(!uplink_id_len)
-    {
-        node->vip_pkt->uplink_id = calloc(50, sizeof(char));
-        memcpy(node->vip_pkt->uplink_id, vip_pkt->uplink_id, uplink_id_len);
-    }
+    // /* uplink */
+    // int uplink_id_len = strlen(vip_pkt->uplink_id);
+    // if(!uplink_id_len)
+    // {
+    //     node->vip_pkt->uplink_id = calloc(50, sizeof(char));
+    //     memcpy(node->vip_pkt->uplink_id, vip_pkt->uplink_id, uplink_id_len);
+    // }
     
-    /* serialized buffer */
-    node->vip_pkt->total_len = vip_pkt->total_len;
-    node->vip_pkt->buffer = calloc(50, sizeof(uint8_t));
-    memcpy(node->vip_pkt->buffer, vip_pkt->buffer, vip_pkt->total_len);
+    // /* serialized buffer */
+    // node->vip_pkt->total_len = vip_pkt->total_len;
+    // node->vip_pkt->buffer = calloc(50, sizeof(uint8_t));
+    // memcpy(node->vip_pkt->buffer, vip_pkt->buffer, vip_pkt->total_len);
     
-    /* con, non type flag */
-    node->vip_pkt->re_flag = vip_pkt->re_flag;
+    // /* con, non type flag */
+    // node->vip_pkt->re_flag = vip_pkt->re_flag;
 
     return node;
 }
