a1ced5594fb838543f4262a08aa7575ca12beb59
soyongkim <thdyd324@gmail.com>
2020-05-26 09:38:18 +0900

    Update res-aa.c

--- examples/vlc-module/resources/res-aa.c
+++ examples/vlc-module/resources/res-aa.c
@@ -178,7 +178,7 @@ handler_vrr(vip_message_t *rcv_pkt) {
     vip_set_dest_ep_cooja(rcv_pkt, dest_addr, VIP_VG_ID, VIP_VG_URL);
     vip_set_type_header_nonce(rcv_pkt, nonce);
     vip_serialize_message(rcv_pkt, buffer);
-    //process_post(&aa_process, aa_snd_event, (void *)rcv_pkt);
+    process_post(&aa_process, aa_snd_event, (void *)rcv_pkt);
   }
   else
   {
