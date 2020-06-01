51585b5d1588adc8162671ca643374be2ecc5f18
soyongkim <thdyd324@gmail.com>
2020-06-01 09:26:28 +0900

    test

--- examples/vlc-module/aa.c
+++ examples/vlc-module/aa.c
@@ -88,7 +88,7 @@ timer_callback(void* data)
 static void init()
 {
   int random_incount;
-  random_incount = random_rand() % 20;
+  random_incount = random_rand() % 30;
   printf("Set Send Timer %d\n", random_incount);
 
   ctimer_set(&ct, random_incount, timer_callback, NULL);
