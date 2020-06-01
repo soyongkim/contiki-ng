a56cdd5db1c4f3e67241acd1501b7b6cba4e972c
soyongkim <thdyd324@gmail.com>
2020-06-01 22:13:17 +0900

    Update vt.c

--- examples/vlc-module/vt.c
+++ examples/vlc-module/vt.c
@@ -151,7 +151,7 @@ vip_request() {
     if(snd_pkt->type == VIP_TYPE_VSD)
     {
       int loss_simul_var = random_rand() % 100;
-      if (loss_simul_var >= 10)
+      if (loss_simul_var >= 15)
       {
         printf("SUCCESS!\n");
       }
