7e37fef0e51054d3b5a4bfe76f6d88667f6f4ded
soyongkim <thdyd324@gmail.com>
2020-06-11 17:42:51 +0900

    Update vip-constants.h

--- examples/vlc-module/sdlib/vip/vip-constants.h
+++ examples/vlc-module/sdlib/vip/vip-constants.h
@@ -14,11 +14,11 @@
 #define VIP_MAX_PKT_SIZE 512
 
 /* for simulation */
-#define VIP_WINDOW_SIZE 16
+#define VIP_WINDOW_SIZE 8
 #define VIP_ERROR_RATE 0
-#define VIP_SIMUL_DATA 100
+#define VIP_SIMUL_DATA 50
 
-#define VIP_SEND_JITTER CLOCK_SECOND/16
+#define VIP_SEND_JITTER CLOCK_SECOND/8
 //#define VIP_SEND_JITTER 0
 
 
