ea00e459fef1038875b2515f7992a793ec186eed
soyongkim <thdyd324@gmail.com>
2020-06-16 17:18:07 +0900

    update script

--- examples/vlc-module/sdlib/configure_cooja/vip_simul_script.js
+++ examples/vlc-module/sdlib/configure_cooja/vip_simul_script.js
@@ -1,5 +1,5 @@
-TIMEOUT(10000);
-log.log("first simulation message at time : " + time + "\n");
+TIMEOUT(112000);
+log.log("Stop for Handover situation! after 30sec\n");
 while (true) {
   YIELD(); /* wait for another mote output */
 }
